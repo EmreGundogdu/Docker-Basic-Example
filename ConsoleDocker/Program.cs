2cf185bf588708ae049abde8f151e7c71daa4296
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-05-05 16:19:47 +0300

    Add project files.

---
+++ ConsoleDocker/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleDocker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i<10000)
+            {
+                Console.WriteLine(i);
+                i++;
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
